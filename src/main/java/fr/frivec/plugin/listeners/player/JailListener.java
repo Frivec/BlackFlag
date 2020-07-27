adbf449ee062af0effb6a58f9d13691ba0749375
Frivec <frivec.99@gmail.com>
2020-07-27 22:01:35 +0200

    Added event when the food level change

--- src/main/java/fr/frivec/plugin/listeners/player/JailListener.java
+++ src/main/java/fr/frivec/plugin/listeners/player/JailListener.java
@@ -4,10 +4,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
 import fr.frivec.BlackFlag;
@@ -22,16 +24,20 @@ public class JailListener implements Listener {
 		final Player player = event.getPlayer();
 		final Location to = event.getTo();
 		
-		if(to.getWorld() != BlackFlag.getInstance().getJailWorld()) {
+		if(BlackFlag.getPlayer(player).isInJail()) {
+		
+			if(to.getWorld() != BlackFlag.getInstance().getJailWorld()) {
+				
+				event.setCancelled(true);
+				player.sendMessage("§cVous ne pouvez pas sortir de prison sans avoir terminé vos objectifs.");
+				
+				return;
+				
+			}else
+				
+				return;
 			
-			event.setCancelled(true);
-			player.sendMessage("§cVous ne pouvez pas sortir de prison sans avoir terminé vos objectifs.");
-			
-			return;
-			
-		}else
-			
-			return;
+		}
 		
 	}
 	
@@ -61,6 +67,14 @@ public class JailListener implements Listener {
 		
 		new ActionBar("§aVous avez miné §b" + bfPlayer.getBlocksBreaked() + "§7/§b" + bfPlayer.getObjective().getNumberOfStack(), 20, 10, 20).send(player);
 		
+		if(bfPlayer.getBlocksBreaked() >= (bfPlayer.getObjective().getNumberOfStack() * 64)) {
+			
+			player.sendMessage("§aBonne nouvelle ! Vous avez purgé votre peine !");
+			player.sendMessage("§6Vous êtes à présent libre ! Téléportation dans le monde d'origine.");
+			player.teleport(new Location(Bukkit.getWorld("world"), 0, 120, 0));
+			
+		}
+		
 		Bukkit.getScheduler().scheduleSyncDelayedTask(BlackFlag.getInstance(), new Runnable() {
 			
 			@Override
@@ -71,6 +85,21 @@ public class JailListener implements Listener {
 			}
 			
 		}, 20 * 30);
+		
+	}
+	
+	@EventHandler
+	public void onFoodLevelChange(final FoodLevelChangeEvent event) {
+		
+		if(event.getEntityType().equals(EntityType.PLAYER)) {
+			
+			final Player player = (Player) event.getEntity();	
+			
+			if(BlackFlag.getPlayer(player).isInJail())
+				
+				player.setFoodLevel(20);
+			
+		}
 		
 	}
 
